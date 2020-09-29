1e13a04707ab8ceb9f0d4621d0764561389e9c3e
CommandrMoose <policebox1@live.com>
2020-09-29 17:55:19 +1000

    Console controls attempt2

--- src/main/java/commandrmoose/master/consoles/positionscale/CopperControlPosScale.java
+++ src/main/java/commandrmoose/master/consoles/positionscale/CopperControlPosScale.java
@@ -1,20 +1,13 @@
-package commandrmoose.master.client.models.consoles.positionscale;
+package commandrmoose.master.consoles.positionscale;
 
 import commandrmoose.master.client.models.consoles.ConsoleControlTransform;
 import commandrmoose.master.tiles.console.CopperConsoleTile;
-import net.minecraft.entity.MoverType;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.server.ServerWorld;
-import net.tardis.mod.controls.BaseControl;
 import net.tardis.mod.controls.CommunicatorControl;
-import net.tardis.mod.controls.IControl;
 import net.tardis.mod.entity.ControlEntity;
-import net.tardis.mod.entity.TEntities;
 import net.tardis.mod.tileentities.ConsoleTile;
-
-import javax.vecmath.Vector3d;
 
 public class CopperControlPosScale extends ConsoleControlTransform {
 
@@ -43,9 +36,7 @@ public class CopperControlPosScale extends ConsoleControlTransform {
 
     static void setControlPos(ConsoleTile console, ControlEntity control, Vec3d offset)
     {
-        BlockPos controlLoc = new BlockPos(console.getPos().getX() + 0.5f + offset.x, console.getPos().getY() + 1f + offset.y,console.getPos().getZ() + 0.5f + offset.z);
-        control.moveToBlockPosAndAngles(console.getPos(), 0, 0);
-        control.moveToBlockPosAndAngles(controlLoc, 0, 0);
+        control.teleportKeepLoaded(console.getPos().getX() + 0.5f + offset.x, console.getPos().getY() + 1f + offset.y,console.getPos().getZ() + 0.5f + offset.z);
     }
 
 }
